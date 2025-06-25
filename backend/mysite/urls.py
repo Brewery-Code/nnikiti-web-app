fe8d42d0ae88e8a522e0893ab324170f7c09030f
mrsy-21 <oleksandr_yakymchuk@protonmail.com>
2025-06-25 15:59:57 +0300

    Added settings for distributing media files (locally)

--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.decorators import permission_classes
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -32,3 +33,6 @@ urlpatterns = [
     path("api/v1/auth/", include("social_django.urls", namespace="social")),
     path("api/v1/auth/token/", include("drf_social_oauth2.urls", namespace="oauth2")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
