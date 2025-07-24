8166c9eedfc738996c78e4deb4da7259301bf99d
Oleksandr Yakymchuk <yakymchuk.oleksandr.dev@gmail.com>
2025-07-24 11:16:40 +0300

    feat(core): Added signal for Alumnus model;

--- backend/core/signals.py
+++ backend/core/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from .models import MainSliderItem, Partner
+from .models import MainSliderItem, Partner, Alumnus
 
 
 @receiver(post_delete, sender=MainSliderItem)
@@ -18,6 +18,16 @@ def delete_main_slider_image(sender, instance, **kwargs):
 def delete_partner_images(sender, instance, **kwargs):
     """Automatically deletes the image file from
     storage when a Partners is deleted.
+    """
+    if instance.image:
+        instance.image.delete(save=False)
+
+
+@receiver(post_delete, sender=Alumnus)
+def delete_alumnus_images(sender, instance, **kwargs):
+    """
+    Automatically deletes the image file from
+    storage when an Alumnus is deleted.
     """
     if instance.image:
         instance.image.delete(save=False)
