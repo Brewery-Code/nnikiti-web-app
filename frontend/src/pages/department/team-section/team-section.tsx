3a5ad15c4a36a47969e1c4289d00f18c29766e5c
Vitalii <x4gm9k@gmail.com>
2025-08-21 23:52:51 +0300

    department page was updated

--- frontend/src/pages/department/team-section/team-section.tsx
+++ frontend/src/pages/department/team-section/team-section.tsx
@@ -1,10 +1,17 @@
 import { Title } from "@/shared/ui";
+import { WorkerCard } from "./worker-card";
 
 export function TeamSection() {
   return (
     <div className="container-base m-section">
       <Title>Team</Title>
-      <div className="mt-16"></div>
+      <div className="grid grid-cols-5 place-items-center gap-8 mt-16">
+        <WorkerCard />
+        <WorkerCard />
+        <WorkerCard />
+        <WorkerCard />
+        <WorkerCard />
+      </div>
     </div>
   );
 }
