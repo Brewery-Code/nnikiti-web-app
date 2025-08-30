dde979e4e31f90c2bfc52fcd793a6a89e55bcd90
Vitalii <x4gm9k@gmail.com>
2025-08-30 23:39:45 +0300

    Updated departments page

--- frontend/src/pages/department/team-section/team-section.tsx
+++ frontend/src/pages/department/team-section/team-section.tsx
@@ -5,7 +5,7 @@ export function TeamSection() {
   return (
     <div className="container-base m-section">
       <Title>Team</Title>
-      <div className="mt-16 grid grid-cols-5 place-items-center gap-8">
+      <div className="mt-16 grid grid-cols-3 place-items-center gap-8">
         <WorkerCard />
         <WorkerCard />
         <WorkerCard />
