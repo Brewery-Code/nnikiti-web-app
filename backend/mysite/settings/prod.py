dc2632962af907d7e0df9fd9a0ef2ce122212b42
Yakymchuk Oleksandr <oleksandr_yakymchuk@protonmail.com>
2026-05-31 19:27:13 +0300

    fix deployment problem

--- backend/mysite/settings/prod.py
+++ backend/mysite/settings/prod.py
@@ -21,8 +21,22 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "mysite" / "static", BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+CLOUDINARY_CLOUD_NAME = config("CLOUD_NAME", default="")
+CLOUDINARY_API_KEY = config("CLOUD_KEY", default="")
+CLOUDINARY_API_SECRET = config("CLOUD_SECRET", default="")
+
+if CLOUDINARY_CLOUD_NAME:
+    INSTALLED_APPS = INSTALLED_APPS + ["cloudinary", "cloudinary_storage"]
+    CLOUDINARY_STORAGE = {
+        "CLOUD_NAME": CLOUDINARY_CLOUD_NAME,
+        "API_KEY": CLOUDINARY_API_KEY,
+        "API_SECRET": CLOUDINARY_API_SECRET,
+    }
+    DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+    MEDIA_URL = f"https://res.cloudinary.com/{CLOUDINARY_CLOUD_NAME}/"
+else:
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = BASE_DIR / "media"
 
 CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", default="").split(",")
 CORS_ALLOW_CREDENTIALS = True
