3ce1e8e8dd8f969704273e1ffa23b35d84f61eca
Yakymchuk Oleksandr <oleksandr_yakymchuk@protonmail.com>
2026-06-02 10:45:28 +0300

    fix migration

--- backend/gallery/migrations/0001_initial.py
+++ backend/gallery/migrations/0001_initial.py
@@ -11,9 +11,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ("parler", "0001_initial"),
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
